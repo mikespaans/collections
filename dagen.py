0f43d78faf65687c8120356259ece49044901bff
Mike Spaans <99069233@mydavinci.nl>
2021-11-15 13:04:21 +0100

    Dagen gemaakt

---
+++ dagen.py
@@ -0,0 +1,8 @@
+DagenWeek = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag']
+print ("Alle dagen van de week zijn: " , DagenWeek)
+print ("De werk dagen van de week zijn: " , DagenWeek[ :5])
+print ("De Weekend dagen zijn: " , DagenWeek[ 5:])
+DagenWeek.reverse()
+print ("Alle dagen in de week in de omgekeerde volgorde zijn: " , DagenWeek)
+print ("De Werkdagen in de omgekeerde volgorde zijn: " , DagenWeek[ 2:])
+print ("De weekend dagen in de omgekeerde volgorde zijn: " , DagenWeek[ :2])
