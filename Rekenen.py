15c37f7f9cefc97961c93f703671300939c5c674
Mike Spaans <99069233@mydavinci.nl>
2021-11-22 09:34:16 +0100

    Spelprogramma

--- Rekenen.py
+++ Rekenen.py
@@ -2,7 +2,11 @@ ListOne = [1,2,3,4,5,6,7,8,9,10]
 ListTwo = [2,4,6,8,10,12,14,16,18,20]
 
 def addAndDisplayLists(list1,list2):
-    for x in ListOne:
-        print (list1 + list2)
+    for Getal1 in ListOne:
+        print 
+    for Getal2 in ListTwo:
+        print (Getal1 + Getal2)
+    
+        
 
 addAndDisplayLists(ListOne, ListTwo)
--- Spelprogramma.py
+++ Spelprogramma.py
@@ -1,8 +1,7 @@
 import random
 AantalSpellen = random.randint(3,10)
 spelList = ['Monoploly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
-def spelProgramma(spelList):
-    print (spelList[ :AantalSpellen])
+def spelProgramma(spelList, minimum, maximum):
+    print (spelList[minimum :maximum])
 
-spelProgramma(spelList)
-print (AantalSpellen)
+spelProgramma(spelList, AantalSpellen, 10)
