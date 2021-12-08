8a4ab17b4bf247f08594332566ee3851f65e6d4c
Mike Spaans <99069233@mydavinci.nl>
2021-12-08 11:18:15 +0100

    M en M zak

--- M en M zak.py
+++ M en M zak.py
@@ -1,8 +1,20 @@
-random = random.choice
+HoeveelMMS = int(input("Hoeveel M&M's moeten er aan de de worden toegevoegd? "))
+import random
+
+for i in range(HoeveelMMS):
+    random = random.choice('OGBR')
 print (random)
-KleurenZak = ["oranje", "groen", "blauw", "bruin"]
-def MMZak(AantalMM):
+def MMZak(x):
+    KleurenZak = ["oranje", "groen", "blauw", "bruin"]
+    if x == "O":
+        KleurenZak.append("oranje")
+    elif x == "G":
+        KleurenZak.append("groen")
+    elif x == "B":
+        KleurenZak.append("blauw")
+    elif x == "R":
+        KleurenZak.append("bruin")
     print (KleurenZak)
 
-#MMZak()
-print (KleurenZak.count())
+
+MMZak(random)
--- Rekenen.py
+++ Rekenen.py
@@ -3,17 +3,24 @@ ListTwo = [2,4,6,8,10,12,14,16,18,20]
 
 def addAndDisplayLists(list1,list2):
     for Getal1 in ListOne:
-        print (Getal1 + Getal2)
-    for Getal2 in ListTwo:
         print 
+    for Getal2 in ListTwo:
+        print (Getal1 + Getal2)
 
 def substractAndDisplayLists(list1,list2):
     for Getal1 in ListOne:
         print 
     for Getal2 in ListTwo:
-        print (Getal1 - Getal2)
+        print (Getal2 - Getal1)
+    
+def multiplyAndDisplayLists(list1,list2):
+    for Getal1 in ListOne:
+        print 
+    for Getal2 in ListTwo:
+        print (Getal2 * Getal1)
     
         
 
 addAndDisplayLists(ListOne, ListTwo)
-# substractAndDisplayLists(ListOne, ListTwo)
+substractAndDisplayLists(ListOne, ListTwo)
+multiplyAndDisplayLists(ListOne, ListTwo)
