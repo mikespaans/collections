ec1f7c0c845aeb10f27717bebfd969b0513e065a
Mike Spaans <99069233@mydavinci.nl>
2022-01-21 12:54:42 +0100

    Rekenen verbeterd

--- Rekenen.py
+++ Rekenen.py
@@ -1,30 +1,26 @@
 ListOne = [1,2,3,4,5,6,7,8,9,10]
 ListTwo = [2,4,6,8,10,12,14,16,18,20]
 
-# def addAndDisplayLists(list1,list2):
-#     for Getal1 in ListOne:
-#         print 
-#     for Getal2 in ListTwo:
-#         print (Getal1 + Getal2)
 
-# def substractAndDisplayLists(list1,list2):
-#     for Getal1 in ListOne:
-#         print 
-#     for Getal2 in ListTwo:
-#         print (Getal2 - Getal1)
-    
-# def multiplyAndDisplayLists(list1,list2):
-#     for Getal1 in ListOne:
-#         print 
-#     for Getal2 in ListTwo:
-#         print (Getal2 * Getal1)
     
 
 def plus(list1, list2):
-    for i in range(10):
+    for i in range(len(list1)):
         print(list1[i] + list2[i])
 plus(ListOne, ListTwo)
 
-# addAndDisplayLists(ListOne, ListTwo)
-# substractAndDisplayLists(ListOne, ListTwo)
-# multiplyAndDisplayLists(ListOne, ListTwo)
+def min(list1, list2):
+    for i in range(len(list1)):
+        print(list1[i] - list2[i])
+min(ListOne, ListTwo)
+
+def keer(list1, list2):
+    for i in range(len(list1)):
+        print(list1[i] * list2[i])
+keer(ListOne, ListTwo)
+
+def gedeeld(list1, list2):
+    for i in range(len(list1)):
+        print(list1[i] / list2[i])
+gedeeld(ListOne, ListTwo)
+
